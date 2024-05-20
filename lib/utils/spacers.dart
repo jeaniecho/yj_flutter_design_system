2abded83a74271442ccf3aaf03465a8545a96aa3
Jeanie <whdpwls63@gmail.com>
2024-05-20 18:15:52 +0900

    Update spacers.dart

---
+++ lib/utils/spacers.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class YJSpacers {
+  static const zero = SizedBox.shrink();
+
+  static const height1 = SizedBox(height: 1);
+  static const height2 = SizedBox(height: 2);
+  static const height4 = SizedBox(height: 4);
+  static const height8 = SizedBox(height: 8);
+  static const height12 = SizedBox(height: 12);
+  static const height16 = SizedBox(height: 16);
+  static const height20 = SizedBox(height: 20);
+  static const height24 = SizedBox(height: 24);
+  static const height32 = SizedBox(height: 32);
+  static const height40 = SizedBox(height: 40);
+  static const height48 = SizedBox(height: 48);
+  static const height56 = SizedBox(height: 56);
+  static const height64 = SizedBox(height: 64);
+  static const height80 = SizedBox(height: 80);
+
+  static const width1 = SizedBox(width: 1);
+  static const width2 = SizedBox(width: 2);
+  static const width4 = SizedBox(width: 4);
+  static const width8 = SizedBox(width: 8);
+  static const width12 = SizedBox(width: 12);
+  static const width16 = SizedBox(width: 16);
+  static const width20 = SizedBox(width: 20);
+  static const width24 = SizedBox(width: 24);
+  static const width32 = SizedBox(width: 32);
+  static const width40 = SizedBox(width: 40);
+  static const width48 = SizedBox(width: 48);
+  static const width56 = SizedBox(width: 56);
+  static const width64 = SizedBox(width: 64);
+}
