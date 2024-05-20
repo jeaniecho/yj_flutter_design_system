21d0ff5646e659ca60585896f30970996e52926e
Jeanie <whdpwls63@gmail.com>
2024-05-20 18:59:32 +0900

    Create border_radius.dart

---
+++ lib/utils/border_radius.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class YJBorderRadius {
+  static final zero = BorderRadius.circular(0);
+
+  static final circular2 = BorderRadius.circular(2);
+  static final circular4 = BorderRadius.circular(4);
+  static final circular6 = BorderRadius.circular(6);
+  static final circular8 = BorderRadius.circular(8);
+  static final circular12 = BorderRadius.circular(12);
+  static final circular16 = BorderRadius.circular(16);
+  static final circular20 = BorderRadius.circular(20);
+  static final circular24 = BorderRadius.circular(24);
+  static final circular32 = BorderRadius.circular(32);
+  static final circular40 = BorderRadius.circular(40);
+  static final circularMax = BorderRadius.circular(9999);
+
+  static const bottom4 =
+      BorderRadius.vertical(bottom: Radius.circular(4), top: Radius.zero);
+  static const bottom8 =
+      BorderRadius.vertical(bottom: Radius.circular(8), top: Radius.zero);
+  static const bottom12 =
+      BorderRadius.vertical(bottom: Radius.circular(12), top: Radius.zero);
+  static const bottom16 =
+      BorderRadius.vertical(bottom: Radius.circular(16), top: Radius.zero);
+  static const bottom20 =
+      BorderRadius.vertical(bottom: Radius.circular(20), top: Radius.zero);
+  static const bottom24 =
+      BorderRadius.vertical(bottom: Radius.circular(24), top: Radius.zero);
+  static const bottom32 =
+      BorderRadius.vertical(bottom: Radius.circular(32), top: Radius.zero);
+
+  static const top4 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(4));
+  static const top8 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(8));
+  static const top12 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(12));
+  static const top16 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(16));
+  static const top20 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(20));
+  static const top24 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(24));
+  static const top32 =
+      BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(32));
+
+  static const left4 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(4));
+  static const left8 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(8));
+  static const left12 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(12));
+  static const left16 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(16));
+  static const left20 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(20));
+  static const left24 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(24));
+  static const left32 =
+      BorderRadius.horizontal(right: Radius.zero, left: Radius.circular(32));
+
+  static const right4 =
+      BorderRadius.horizontal(right: Radius.circular(4), left: Radius.zero);
+  static const right8 =
+      BorderRadius.horizontal(right: Radius.circular(8), left: Radius.zero);
+  static const right12 =
+      BorderRadius.horizontal(right: Radius.circular(12), left: Radius.zero);
+  static const right16 =
+      BorderRadius.horizontal(right: Radius.circular(16), left: Radius.zero);
+  static const right20 =
+      BorderRadius.horizontal(right: Radius.circular(20), left: Radius.zero);
+  static const right24 =
+      BorderRadius.horizontal(right: Radius.circular(24), left: Radius.zero);
+  static const right32 =
+      BorderRadius.horizontal(right: Radius.circular(32), left: Radius.zero);
+}
